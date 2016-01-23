28c85c4e57f0b71557718618b34b5ae9057e9e55
marius.nick <marius.nick@gmail.com>
2016-01-23 09:32:56 +0200

    second commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,23 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## functions do:
 
 ## The function makeCacheMatrix creates a object matrix what will be cache
+## inv is the cached value of the inverse
+## x is the current matrix whose inverse is being evaluated
 
 makeCacheMatrix <- function(x = matrix()) {
 
-inv<-NULL
-set<-fuction(y)
-{<<-            x <<- y
- inv<<- NULL
-}
-get <- function() x
-setrevese<- function(reverse) inv <<- reverse()
-getreverse <- function() inv
-list(set = set, get = get,
-     setreverse = setreverse,
-     getreverse = getreverse) }
+      inv<-NULL
+      set<-function(y){  
+            x <<- y
+            inv<<- NULL
+      }
+      get <- function() x
+      setrevese<- function(reverse) inv <<- reverse()
+      getreverse <- function() inv
+      list(set = set, get = get,
+       setreverse = setreverse,
+       getreverse = getreverse) 
 }
 
 
